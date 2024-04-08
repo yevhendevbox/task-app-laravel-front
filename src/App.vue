1b24b7014bd75457f63655d9cd8451f0819afb07
Yevhen Dovhan <y.d.devbox@gmail.com>
2024-04-08 12:24:53 +0300

    feat: base ui components added

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,69 @@
 <script setup>
+import { ref, onMounted } from "vue";
+import { columns } from "@/components/ui/data-table/columns";
+import DataTable from "@/components/ui/data-table/DataTable.vue";
+
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Plus } from "lucide-vue-next";
+import {
+  NavigationMenu,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
+
+const data = ref([]);
+
+async function getData() {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    // ...
+  ];
+}
+
+onMounted(async () => {
+  data.value = await getData();
+});
 </script>
 
 <template>
-  <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-    Hello from App.vue
-  </h1>
-  <main>
-    <Button>Test btn</Button>
+  <NavigationMenu class="bg-zinc-900 min-w-full">
+    <div class="container py-[2rem]">
+      <NavigationMenuList class="flex justify-between">
+        <NavigationMenuLink class="text-xl">Todo List App</NavigationMenuLink>
+        <div class="flex gap-4">
+          <Button variant="outline"> Login </Button>
+          <Button>Register</Button>
+          <Button variant="outline"> Logout </Button>
+        </div>
+      </NavigationMenuList>
+    </div>
+  </NavigationMenu>
+
+  <main class="container mx-auto py-[4rem]">
+    <div class="relative w-full max-w-sm items-center">
+      <Input
+        id="add"
+        type="text"
+        placeholder="Add new task. Please enter to save"
+        class="pl-10"
+      />
+      <span
+        class="absolute start-0 inset-y-0 flex items-center justify-center px-2"
+      >
+        <Plus class="size-6 text-muted-foreground" />
+      </span>
+    </div>
+
+    <div class="py-10 mx-auto">
+      <DataTable :columns="columns" :data="data" />
+    </div>
   </main>
 </template>
 
---
+++ src/components/ui/data-table/columns.js
@@ -0,0 +1,17 @@
+import { h } from 'vue'
+
+export const columns = [
+  {
+    accessorKey: 'amount',
+    header: () => h('div', { class: 'text-right' }, 'Amount'),
+    cell: ({ row }) => {
+      const amount = Number.parseFloat(row.getValue('amount'))
+      const formatted = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+      }).format(amount)
+
+      return h('div', { class: 'text-right font-medium' }, formatted)
+    },
+  }
+]
